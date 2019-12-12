c302ef46fecc343688b2f359c3642f7af8bd196a
cedric e goddard <8ceg6@comcast.net>
2019-12-12 03:00:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,10 @@
 #write your code here
 
-def countdown(number)
-
+def countdown(n)
+    while n > 0
+      sleep 1
+      puts "#{number} SECOND(S)!"
+      n -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
